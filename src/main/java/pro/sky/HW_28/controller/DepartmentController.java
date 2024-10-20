e3fdbdf8d4b17042942c412d274347b28de7542a
Cranky Bawler <6666lenia6666@mail.ru>
2024-10-20 10:23:31 +0300

    1

--- src/main/java/pro/sky/HW_28/controller/DepartmentController.java
+++ src/main/java/pro/sky/HW_28/controller/DepartmentController.java
@@ -1,11 +1,11 @@
 package pro.sky.HW_28.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pro.sky.HW_28.entity.Employee;
 import pro.sky.HW_28.service.DepartmentService;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/departments")
@@ -20,5 +20,20 @@ public class DepartmentController {
     @GetMapping("/max-salary")
     public Employee maxSalary(@RequestParam int departmentId) {
         return departmentService.maxSalary(departmentId);
+    }
+
+    @GetMapping("/min-salary")
+    public Employee minSalary(@RequestParam int departmentId) {
+        return departmentService.minSalary(departmentId);
+    }
+
+    @GetMapping("/all")
+    public List <Employee> findForDepartment(@RequestParam int departmentId) {
+        return (List<Employee>) departmentService.findForDepartment(departmentId);
+    }
+
+    @GetMapping("/all")
+    public Map <Integer, List<Employee>> findAllForDepartment (){
+        return departmentService.findAll ();
     }
 }
--- src/main/java/pro/sky/HW_28/service/DepartmentService.java
+++ src/main/java/pro/sky/HW_28/service/DepartmentService.java
@@ -1,4 +1,17 @@
 package pro.sky.HW_28.service;
 
+import pro.sky.HW_28.entity.Employee;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
 public interface DepartmentService {
+    Employee maxSalary(int departmentId);
+
+    Employee minSalary(int departmentId);
+
+    Collection<Employee> findForDepartment(int departmentId);
+
+    Collection<Employee> findAll();
 }
